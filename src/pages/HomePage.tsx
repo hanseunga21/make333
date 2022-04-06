2b5a31dbf4d4faba9b0e2c3b93f3339de589da05
hanseunga21 <hanseunga@ewhain.net>
2022-04-06 09:51:42 +0900

    done

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,22 @@
 import React from 'react';
+import { TextField } from '../components/TextField';
 
 export const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+  <div> className="m-4"
+    <div className="text-3xl font-bold mb-10">List</div>
+
+    <div className="py-40 bg-gray-200 text-gray-800 text-left font-bold text-2xl">
+      </div>
+    <div className="mt-5 text-3xl font-bold mb-10">Card</div>
+
+    <div>
+      <div className="bg-gray-800 text-white text-center py4">만들기</div>
+      <div className="text-center py-4 text-gray-800 border border-gray-800">
+        공유하기
+      </div>
+    </div>
+
+  </div>
+  );
 };
--- src/pages/Router.tsx
+++ src/pages/Router.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Redirect, Route, Switch } from 'react-router';
 import { LoginPage } from './LoginPage';
 import { HomePage } from './HomePage';
-import { SignupPage } from './SignupPage';
+import { SignupPage } from './SignupPage'
 
 export const Router = () => {
   return (
